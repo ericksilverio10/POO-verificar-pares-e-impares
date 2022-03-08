6aedb1a35af2b18e57f66645d1b72fbfc42c1a90
Erick <erick1junior@hotmail.com>
2022-03-08 01:51:38 -0300

    Add files

---
+++ verificador-pares-impares.js
@@ -0,0 +1,47 @@
+//---------------------------------------------------------
+//Verificador de números consecutivos Pares e Ímpares:
+
+class VerificarNumero{
+    constructor(n){
+        this.num = n;
+    }
+    verificarImpares(qtd){
+        console.log(`Os ${qtd} números ímpares consecutivos de ${this.num} são:`)
+        for(let i = 0; i <= qtd*2; i++)
+        {
+            if(i % 2 != 0 && this.num % 2 == 0){
+                console.log(i + this.num)
+            } else if(i % 2 != 0 && this.num % 2 != 0){
+                console.log(i + this.num + 1)
+            }
+        }
+        console.log("PRONTO!");
+    }
+    verificarPares(qtd){
+        console.log(`Os ${qtd} números pares consecutivos de ${this.num} são:`)
+        for(let i = 0; i <= qtd*2; i++)
+        {
+            if(i % 2 == 0 && this.num % 2 == 0){
+                console.log(i + this.num)
+            } else if(i % 2 == 0 && this.num % 2 != 0){
+                console.log(i + this.num + 1)
+            }
+        }
+        console.log("PRONTO!");
+    }
+}
+const segundoNumero = new VerificarNumero(3);
+segundoNumero.verificarPares(10)
+// Os 10 números pares consecutivos de 3 são:
+// 4
+// 6
+// 8
+// 10
+// 12
+// 14
+// 16
+// 18
+// 20
+// 22
+// 24
+// PRONTO!
